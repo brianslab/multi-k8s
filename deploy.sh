0ec7ce5262f17ffeb7307c8e969f50bd2c20c63d
brianslab <bdkuhn602@gmail.com>
2021-07-08 21:20:51 -0700

    reverted deploy.sh to use loops

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,11 @@
-docker build -t brianslab/multi-client:latest -t brianslab/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t brianslab/multi-server:latest -t brianslab/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t brianslab/multi-worker:latest -t brianslab/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-
-docker push brianslab/multi-client:latest
-docker push brianslab/multi-server:latest
-docker push brianslab/multi-worker:latest
-
-docker push brianslab/multi-client:$SHA
-docker push brianslab/multi-server:$SHA
-docker push brianslab/multi-worker:$SHA
+for image in client server worker; do 
+    docker build -t brianslab/multi-$image:latest -t brianslab/multi-$image:$SHA -f ./$image/Dockerfile ./$image
+    docker push brianslab/multi-$image:latest
+    docker push brianslab/multi-$image:$SHA
+done
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=brianslab/multi-server:$SHA
-kubectl set image deployments/client-deployment client=brianslab/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=brianslab/multi-worker:$SHA
+
+for image in client server worker do
+    kubectl set image deployments/$image-deployment server=brianslab/multi-$image:$SHA
+done
